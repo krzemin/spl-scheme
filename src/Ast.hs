ae72d266333a9e8e9bf61ff8ecddd76747788845
Błażej Saladra <bazusaladra@gmail.com>
2013-12-21 19:59:15 +0100

    Ast names change

--- src/Ast.hs
+++ src/Ast.hs
@@ -1,16 +1,16 @@
 module Ast where
 
 
-data SchemeAst = LeafNumber Int
-               | LeafAtom String
-               | LeafString String
+data SchemeAst = Number Int
+               | Atom String
+               | Str String
                | List [SchemeAst]
                deriving (Eq)
 
 instance Show SchemeAst where
-  show (LeafNumber n) = show n
-  show (LeafAtom x) = x
-  show (LeafString s) = "\"" ++ s ++ "\""
+  show (Number n) = show n
+  show (Atom x) = x
+  show (Str s) = "\"" ++ s ++ "\""
   show (List []) = "()"
   show (List (x:xs)) = (foldl (\acc y -> acc ++ " " ++ show y) ("(" ++ show x) xs) ++ ")" 
 
