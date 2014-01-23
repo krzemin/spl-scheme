9d5df4cd5cdb3257da2b88ee377e5216f33902c0
Piotr Krzeminski <pio.krzeminski@gmail.com>
2014-01-23 03:23:47 +0100

    do-notation with continuations reworked out; Val made parametric of Expr type; Val now instantiaded as a Monad

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,15 +1,31 @@
 module Eval where
 
-import  Prelude hiding (lookup)
-import  Expr
-import  Data.Map hiding (foldl)
+import Prelude hiding (lookup)
+import Expr
+import Types
+import Data.Map hiding (foldl)
 
-data Val = OK Expr | Err String | TypeErr String
+
+data Val expr = OK expr | Err String | TypeErr String
 type Env = Map String Expr
-type Cont = Expr -> Val
-type TypedCont repr = repr -> Val
+type Cont = Expr -> Val Expr
 
-evalExpr :: Expr -> Cont -> Env -> Val
+
+instance Monad Val where
+  return = OK
+  (OK e) >>= cont = cont e
+  (Err s) >>= _ = Err s
+  (TypeErr s) >>= _ = TypeErr s 
+
+
+evalTyped :: TypeDef repr -> Expr -> Env -> Val Expr
+evalTyped t e env = case (toRepr t) e of 
+  Just _ -> evalExpr e OK env
+  Nothing -> TypeErr $ "Expected type `" ++ name t ++ "`, but given " ++ show e 
+
+
+evalExpr :: Expr -> Cont -> Env -> Val Expr
+
 evalExpr (Num n) k _ = k $ Num n
 evalExpr (Bool b) k _ = k $ Bool b
 evalExpr (Str s) k _ = k $ Str s
@@ -18,80 +34,59 @@ evalExpr (Atom a) k env = case lookup a env of
   Nothing -> Err $ "Name `" ++ a ++ "` was not bound in current scope."
 evalExpr (List ls) k env = evalList ls k env
 
-evalList :: [Expr] -> Cont -> Env -> Val
-evalList (Atom "+" : e0 : e1 : []) k env = evalExpr e0 k' env where
-  k' = typedCont numType (\n0 -> evalExpr e1 (k'' n0) env)
-  k'' n0 = typedCont numType (\n1 -> k $ Num (n0 + n1))
 
-evalTyped :: TypeDef repr -> Expr -> TypedCont a -> Env -> Val
-evalTyped t e k env = let reprE = (toRepr t) e in k reprE
+evalList :: [Expr] -> Cont -> Env -> Val Expr
 
---evalList (Atom "-" : e0 : e1 : []) k env = do
---  n0 <- evalTyped numType e0 env
---  n1 <- evalTyped numType e1 env
---  k $ Num $ n0 + n1
+evalList (Atom "+" : e0 : e1 : []) k env = do
+  v0 <- evalTyped numType e0 env
+  let n0 = extractVal numType v0
+  v1 <- evalTyped numType e1 env
+  let n1 = extractVal numType v1
+  k $ Num (n0 + n1)
 
+evalList (Atom "-" : e0 : e1 : []) k env = do
+  v0 <- evalTyped numType e0 env
+  let n0 = extractVal numType v0
+  v1 <- evalTyped numType e1 env
+  let n1 = extractVal numType v1
+  k $ Num (n0 - n1)
 
---evalList (Atom "*" : e0 : e1 : []) k env =
---  evalTyped numType e0 env >>= (\n0 ->
---    evalTyped numType e1 env >>= (\n1 ->
---      k $ Num $ n0 + n1
---    )
---  )
+evalList (Atom "*" : e0 : e1 : []) k env = do
+  v0 <- evalTyped numType e0 env
+  let n0 = extractVal numType v0
+  v1 <- evalTyped numType e1 env
+  let n1 = extractVal numType v1
+  k $ Num (n0 + n1)
 
+evalList (Atom "/" : e0 : e1 : []) k env = do
+  v0 <- evalTyped numType e0 env
+  let n0 = extractVal numType v0
+  v1 <- evalTyped numType e1 env
+  let n1 = extractVal numType v1
+  if n1 == 0 then Err "Division by zero." else k $ Num (n0 `div` n1)
 
+evalList (Atom "%" : e0 : e1 : []) k env = do
+  v0 <- evalTyped numType e0 env
+  let n0 = extractVal numType v0
+  v1 <- evalTyped numType e1 env
+  let n1 = extractVal numType v1
+  if n1 == 0 then Err "Division by zero." else k $ Num (n0 `mod` n1)
 
+evalList (Atom "not" : e : []) k env = do
+  v <- evalTyped boolType e env
+  let b = extractVal boolType v
+  k $ Bool (not b)
 
---instance Monad TypedCont where
---  return :: repr -> TypedCont repr
---  return v = OK $ fromRepr v
---  >>= :: TypedCont reprA -> (reprA -> TypedCont reprB) -> TypedCont reprB
---  f >>= g = g f 
+evalList (Atom "cond" : b : e0 : e1 : []) k env = do
+  v <- evalTyped boolType b env
+  let r = extractVal boolType v
+  if r then evalExpr e0 k env else evalExpr e1 k env
 
+evalList (Atom "quote" : e : []) k _ = k e
 
---evalList (Atom "not" : e : []) k env = evalExpr e k' env where
---  k' = typedCont boolType (\v -> k $ Bool $ not v)
-
-
---evalList ls _ _ = Err $ "Sorry. This is not valid SPL-Scheme expression:\n" ++ show (List ls)
-
-
-data TypeDef repr = TypeDef {
-  name :: String,
-  toRepr :: Expr -> Maybe repr,
-  fromRepr :: repr -> Expr
-} 
-
-typedCont :: TypeDef repr -> TypedCont repr -> Expr -> Val
-typedCont t k e = case (toRepr t) e of
-  Just v -> k v
-  Nothing -> TypeErr $ "Expected type `" ++ name t ++ "`, but given " ++ show e 
-
-
-numType :: TypeDef Int
-numType = TypeDef "int" exFun Num where
-  exFun (Num n) = Just n
-  exFun _ = Nothing
-
-boolType :: TypeDef Bool
-boolType = TypeDef "bool" exFun Bool where
-  exFun (Bool b) = Just b
-  exFun _ = Nothing
-
---consType :: TypeDef
---consType = TypeDef "cons" exFun where
---  ckFun (List [Atom "cons", _, _]) = True
---  ckFun _ = False
-
---funType :: TypeDef
---funType = TypeDef "fun" ckFun where
---  ckFun (List [Atom "lambda", _, _]) = True
---  ckFun _ = False
+evalList ls _ _ =
+  Err $ "Sorry. This is not valid SPL-Scheme expression:\n" ++ show (List ls)
 
 
-
-
-
-
-eval :: Expr -> Val
+eval :: Expr -> Val Expr
 eval e = evalExpr e OK empty
