fbf5cf429689ad17290d314fdf8c414be34584d4
Piotr Krzeminski <pio.krzeminski@gmail.com>
2014-01-23 03:53:04 +0100

    REPL rewritten with Readline library

--- src/ReplMain.hs
+++ src/ReplMain.hs
@@ -1,26 +1,28 @@
 module ReplMain where
 
-import System.IO
 import Parser
 import Eval
+import System.Console.Readline
 
 main :: IO ()
 main = do
   putStrLn "Welcome to spl-scheme REPL!"
-  loop
+  repl
    
-loop :: IO ()
-loop = do
-  putStr "> "
-  hFlush stdout
-  line <- getLine
-  case parseScheme line of
-    Right expr -> do
-      print expr
-      case eval expr of
-        OK e -> print e
-        Err s -> putStrLn s
-        TypeErr s -> putStrLn s
-      loop
-    Left err ->
-      putStrLn err
+repl :: IO ()
+repl = do
+  maybeLine <- readline "> "
+  case maybeLine of
+    Nothing     -> return ()
+    Just "exit" -> return ()
+    Just line   -> do
+      addHistory line
+      case parseScheme line of
+        Right expr -> do
+          case eval expr of
+            OK e -> print e
+            Err s -> putStrLn $ "Runtime error: " ++ s
+            TypeErr s -> putStrLn $ "Type error: " ++ s
+        Left e ->
+          putStrLn $ "Parse error: " ++ e
+      repl
