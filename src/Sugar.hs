5ee99a105890eb0287b47c56ac2dd7fbd1f5e119
Piotr Krzemiński <pio.krzeminski@gmail.com>
2014-01-25 19:02:11 +0100

    desugaring function definition

--- src/Sugar.hs
+++ src/Sugar.hs
@@ -13,12 +13,17 @@ desugar (List [Atom "let", Atom x, e0, e1]) =
   List [List [Atom "lambda", Atom x, desugar e1], desugar e0]
 
 -- desugaring multi-argument lambdas
-desugar (List [Atom "lambda", List [Atom x], e]) =
-  List [Atom "lambda", Atom x, desugar e]
+desugar (List [Atom "lambda", List [], e]) = desugar e
 desugar (List [Atom "lambda", List (Atom x : xs), e]) =
   List [Atom "lambda", Atom x, desugar e']
   where
     e' = List [Atom "lambda", List xs, e]
+
+-- desugaring function definition
+desugar (List [Atom "defun", List (Atom f : args), e]) =
+	List [Atom "define", Atom f, desugar lam]
+	where
+		lam = List [Atom "lambda", List args, e]
 
 desugar (List xs) = List $ map desugar xs
 
