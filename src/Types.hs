8a04a8739f34b1daececf021cc6875160b28aa4f
Piotr Krzeminski <pio.krzeminski@gmail.com>
2014-02-10 22:27:24 +0100

    fromRepr removed

--- src/Types.hs
+++ src/Types.hs
@@ -4,8 +4,7 @@ import Expr
 
 data TypeDef repr = TypeDef {
   name :: String,
-  toRepr :: Expr -> Maybe repr,
-  fromRepr :: repr -> Expr
+  toRepr :: Expr -> Maybe repr
 }
 
 typed :: TypeDef repr -> (repr -> Val) -> Cont
@@ -14,34 +13,32 @@ typed t cont _ val =
     Just v -> cont v
     Nothing -> TypeErr $ "Expected type `" ++ name t ++ "`, given " ++ show val
 
-
 numType :: TypeDef Int
-numType = TypeDef "num" extract Num where
+numType = TypeDef "num" extract where
   extract (Num n) = Just n
   extract _ = Nothing
 
 boolType :: TypeDef Bool
-boolType = TypeDef "bool" extract Bool where
+boolType = TypeDef "bool" extract where
   extract (Bool b) = Just b
   extract _ = Nothing
 
 atomType :: TypeDef String
-atomType = TypeDef "atom" extract Atom where
+atomType = TypeDef "atom" extract where
   extract (Atom a) = Just a
   extract _ = Nothing
 
 strType :: TypeDef String
-strType = TypeDef "string" extract Atom where
+strType = TypeDef "string" extract where
   extract (Str s) = Just s
   extract _ = Nothing
 
 consType :: TypeDef [Expr]
-consType = TypeDef "cons" extract List where
+consType = TypeDef "cons" extract where
   extract (List ls@[Atom "cons", _, _]) = Just ls
   extract _ = Nothing
 
 cloType :: TypeDef (CloFun)
-cloType = TypeDef "closure" extract consFun where
+cloType = TypeDef "closure" extract where
   extract (Clo fun) = Just fun
   extract _ = Nothing
-  consFun = Clo
